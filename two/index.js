d3b31c50456213e03e20f91de05eae212a099fc5
zhukovka <helen.zhukova@gmail.com>
2018-05-19 21:33:31 +0300

    code cleanup

--- two/index.js
+++ two/index.js
@@ -1,7 +1,7 @@
 import {AudioPlayer} from '../audioPlayer.js';
 
 import {from, fromEvent, timer, zip} from 'rxjs';
-import {concatMap, mapTo} from 'rxjs/operators';
+import {concatMap} from 'rxjs/operators';
 
 let canvasElement = document.getElementById("canvas");
 let audioPlayer = new AudioPlayer();
@@ -13,17 +13,13 @@ const c = beat / 2;
 const march = ['G3', 'G3', 'G3', 'D_3', 'A_3', 'G3', 'D_3', 'A_3', 'G3'];
 const duration = [c, c, c, c, q, c, c, q, beat];
 const length = [0, beat, beat, beat, 2 * beat / 3, beat / 4, beat, 2 * beat / 3, beat / 4, 0];
+let march$ = from(march);
+let duration$ = from(duration);
+let length$ = from(length);
+let midi$ = zip(march$, duration$, length$.pipe(concatMap(ms => timer(ms))));
 
 fromEvent(document, 'click')
-    .pipe(
-        concatMap(() => {
-            let march$ = from(march);
-            let duration$ = from(duration);
-            let length$ = from(length);
-            return zip(march$, duration$, length$.pipe(concatMap(ms => {
-                return timer(ms);
-            })));
-        }))
+    .pipe(concatMap(() => midi$))
     .subscribe((zipped) => {
         let [note, ms] = zipped;
         audioPlayer.play(note, 0, ms / 1000);
