557ac4b562756c37cd3c8287b60c22d7955c6603
zhukovka <helen.zhukova@gmail.com>
2018-05-23 10:51:39 +0300

    only dist

--- index.js
+++
@@ -1,7 +0,0 @@
-const next = document.getElementById("next");
-document.addEventListener('keypress', e => {
-    console.log('press', e);
-    if (e.code === "Enter") {
-        open(next.href,"_self");
-    }
-});
